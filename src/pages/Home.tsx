fbe4fd598202e29dd38230314a5406d5abf899f0
SinanovicAhmed <ahmedsinanovic20@gmail.com>
2023-06-21 11:33:13 +0200

    load more func using useInfiniteQuery

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,53 +1,38 @@
-import { useQuery } from "@tanstack/react-query";
-import React, { useContext, useEffect, useState } from "react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import React, { useContext } from "react";
 import { getNews } from "../api/api";
-import { ITopNews } from "../interfaces/news";
 import { Header } from "../components/Header/Header";
 import { CountryContext } from "../context/context";
 import { HomeNews } from "../components/NewsDisplay/HomeNews";
-const params = {
-  endpoint: "/top-headlines",
-  options: { country: "us", page: 1 },
-};
 
 export const Home: React.FC = () => {
   const { country } = useContext(CountryContext);
-  const [news, setNews] = useState<ITopNews[]>([]);
-  const [newsParams, setNewsParams] = useState(params);
-  const { isLoading, isError, error, data } = useQuery(
-    ["topNews", newsParams.options.page, newsParams.options.country],
-    () => getNews(newsParams),
+  const params = { endpoint: "/top-headlines", options: { country: country } };
+  const { data, hasNextPage, fetchNextPage } = useInfiniteQuery(
+    [params],
+    ({ pageParam = 0, queryKey }) => getNews(queryKey[0], pageParam),
     {
       refetchOnWindowFocus: false,
-      onSuccess: (data) => setNews((prevState) => [...prevState, ...data?.articles]),
+      getNextPageParam(lastPage, pages) {
+        if (pages.length * 20 < lastPage.totalResults) {
+          return pages.length + 1;
+        } else {
+          return undefined;
+        }
+      },
     }
   );
-  const nextPage = data && news.length < data.totalResults;
-  const loadMore = () => {
-    if (nextPage) {
-      setNewsParams((prevState) => ({
-        ...prevState,
-        options: { ...prevState.options, page: prevState.options.page + 1 },
-      }));
-    }
+  const handleLoadMore = () => {
+    fetchNextPage();
   };
-  /*changing country in params when context is changed which will triger rerender
-  because newsParams.options.country is key in useQuery*/
-  useEffect(() => {
-    setNews([]);
-    setNewsParams((prevParams) => ({
-      ...prevParams,
-      options: { page: 1, country: country },
-    }));
-  }, [country]);
-
+  const allArticles = data?.pages.flatMap((page) => page.articles) ?? [];
   return (
     <div className="w-full">
       <Header />
-      <HomeNews news={news} />
+      <HomeNews news={allArticles} />
       <button
-        onClick={loadMore}
-        disabled={!nextPage}
+        disabled={!hasNextPage}
+        onClick={handleLoadMore}
         className="px-4 py-2 my-10 font-medium bg-red-800 text-white disabled:opacity-25"
       >
         load more...
